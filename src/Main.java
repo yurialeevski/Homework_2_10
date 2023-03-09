eb2305e075080040406f85ba93d78cdf5e2ebd15
ya121 <ya7771@yandex.ru>
2023-03-09 14:28:07 +0300

    Начальный коммит к ДЗ № 2_10

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
+    public static void task1() { System.out.println("Задача 1"); }
+    public static void task2() { System.out.println("Задача 2"); }
+    public static void task3() {
+        System.out.println("Задача 3");
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+    }
+}
